5c17a2df6c3390fac2c942a5374f1cb3d035c816
Neil Gutkin <neilg99@vt.edu>
2022-11-10 20:58:07 -0500

    Added GPU functionality, but there is a small bug, so it's disabled for now.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,24 @@
-FROM python:3
+FROM nvidia/cuda:11.6.0-base-ubuntu18.04
+ARG DEBIAN_FRONTEND=noninteractive
+
+ENV PATH /opt/conda/bin:$PATH
+
+RUN apt-get update --fix-missing && \
+    apt-get install -y wget bzip2 ca-certificates libglib2.0-0 libxext6 libsm6 libxrender1 git mercurial subversion && \
+    apt-get clean
+
+RUN wget --quiet https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh && \
+    /bin/bash ~/miniconda.sh -b -p /opt/conda && \
+    rm ~/miniconda.sh && \
+    /opt/conda/bin/conda clean -tipsy && \
+    ln -s /opt/conda/etc/profile.d/conda.sh /etc/profile.d/conda.sh && \
+    echo ". /opt/conda/etc/profile.d/conda.sh" >> ~/.bashrc && \
+    echo "conda activate base" >> ~/.bashrc && \
+    find /opt/conda/ -follow -type f -name '*.a' -delete && \
+    find /opt/conda/ -follow -type f -name '*.js.map' -delete && \
+    /opt/conda/bin/conda clean -afy
+
+RUN conda install cudatoolkit
 
 WORKDIR /app/src
 
--- gridtools/gridding.py
+++ gridtools/gridding.py
@@ -20,6 +20,8 @@
 
 import numpy as np
 import numpy.ma as ma
+from numba import cuda
+import math
 import sat_data_input
 import filter_data
 
@@ -52,45 +54,169 @@ def grid(limit,gsize,indata,inlat,inlon): #valid_range
     stdtau=np.full([xdim,ydim],-9999.0)
     grdlat=np.full([xdim,ydim],-9999.0)
     grdlon=np.full([xdim,ydim],-9999.0)
-    n=0
-    
-    for ii in range(len(indata)):
-        #check within bounds
-        # indata should be filtered based on range (not 0, 5 but rather valid_range)
-        # if (inlat[ii]>=minlat and inlat[ii] <= maxlat and inlon[ii]>= minlon and inlon[ii]<= maxlon): # and indata[ii] >0.0 and indata[ii]<=5.0):
 
-        if (inlat[ii]>=minlat and inlat[ii] <= maxlat and inlon[ii]>= minlon and inlon[ii]<= maxlon): # and indata[ii] >0.0 and indata[ii]<=5.0):
-            # pixel coordinates for bounds
-            i=int(round((inlon[ii]-minlon)/dx))
-            j=int(round((inlat[ii]-minlat)/dy))
-            
-            # do not take nan values into account
-            # nans should not be counted
-            sumtau[i,j]=sumtau[i,j]+indata[ii]
-            sqrtau[i,j]=sqrtau[i,j]+(indata[ii])**2
-            count[i,j]+=1
-            if indata[ii] < mintau[i,j]:
-                mintau[i,j]=indata[ii]
-            if indata[ii] > maxtau[i,j]:
-                maxtau[i,j]=indata[ii]
+    # Run on CPU if no GPU is available
+    # For now, run on CPU before GPU bug is resolved
+    if (True): #(len(cuda.list_devices()) == 0 or len(indata) == 0)
+        for ii in range(len(indata)):
+            #check within bounds
+            # indata should be filtered based on range (not 0, 5 but rather valid_range)
+            if (inlat[ii]>=minlat and inlat[ii] <= maxlat and inlon[ii]>= minlon and inlon[ii]<= maxlon): # and indata[ii] >0.0 and indata[ii]<=5.0):
+                # pixel coordinates for bounds
+                i=int(round((inlon[ii]-minlon)/dx))
+                j=int(round((inlat[ii]-minlat)/dy))
                 
-            #print("updated: ", sumtau[i,j], mintau[i,j], maxtau[i,j])
-    for i in range(xdim):
-        for j in range(ydim):
-            grdlon[i,j]=dx*i+minlon
-            grdlat[i,j]=dx*j+minlat
-            if count[i,j] > 0:
-                avgtau[i,j]=sumtau[i,j]/count[i,j]
-                para1=(1/count[i,j])*(sqrtau[i,j])+(count[i,j])*avgtau[i,j]-2*(avgtau[i,j])*(sumtau[i,j])
-                if para1 > 0:
-                    stdtau[i,j]=np.sqrt(para1)
+                # do not take nan values into account
+                # nans should not be counted
+                sumtau[i,j]=sumtau[i,j]+indata[ii]
+                sqrtau[i,j]=sqrtau[i,j]+(indata[ii])**2
+                count[i,j]+=1
+                if indata[ii] < mintau[i,j]:
+                    mintau[i,j]=indata[ii]
+                if indata[ii] > maxtau[i,j]:
+                    maxtau[i,j]=indata[ii]
                     
-    # change none to fill values
+                #print("updated: ", sumtau[i,j], mintau[i,j], maxtau[i,j])
+        for i in range(xdim):
+            for j in range(ydim):
+                grdlon[i,j]=dx*i+minlon
+                grdlat[i,j]=dx*j+minlat
+                if count[i,j] > 0:
+                    avgtau[i,j]=sumtau[i,j]/count[i,j]
+                    para1=(1/count[i,j])*(sqrtau[i,j])+(count[i,j])*avgtau[i,j]-2*(avgtau[i,j])*(sumtau[i,j])
+                    if para1 > 0:
+                        stdtau[i,j]=np.sqrt(para1)
+                        
+        # change none to fill values
+        mintau[mintau==10]=None
+        maxtau[maxtau==-1]=None
+        avgtau[avgtau==-1]=None
+
+        return avgtau,stdtau,grdlat,grdlon,mintau,maxtau,count,sumtau
+
+    # Run on GPU if GPU is available
+    # Create device arrays
+    d_sumtau = cuda.to_device(sumtau.flatten())
+    d_sqrtau = cuda.to_device(sqrtau.flatten())
+    d_count = cuda.to_device(count.flatten())
+    d_mintau = cuda.to_device(mintau.flatten())
+    d_maxtau = cuda.to_device(maxtau.flatten())
+    d_avgtau = cuda.to_device(avgtau.flatten())
+    d_stdtau = cuda.to_device(stdtau.flatten())
+    d_grdlat = cuda.to_device(grdlat.flatten())
+    d_grdlon = cuda.to_device(grdlon.flatten())
+    d_indata = cuda.to_device(indata)
+    d_limit = cuda.to_device(limit)
+    d_inlat = cuda.to_device(inlat)
+    d_inlon = cuda.to_device(inlon)
+
+    # Run first kernel
+    tpb = (256)
+    bpg = (int(np.ceil(len(indata) / tpb)))
+    print("indata " + str(len(indata)))
+    print("tpb " + str(tpb))
+    print("bpg " + str(bpg))
+    grid_gpu_1[bpg, tpb](gsize, ydim, len(indata),\
+        d_indata, d_limit, d_inlat, d_inlon, d_sumtau, d_count, d_mintau, d_maxtau)
+
+    # Run second kernel
+    # Threads per block
+    tpb = (16, 16)
+    # Blocks per grid
+    bpg_x = int(np.ceil(xdim / tpb[0]))
+    bpg_y = int(np.ceil(ydim / tpb[1]))
+    bpg = (bpg_x, bpg_y)
+    grid_gpu_2[bpg, tpb](gsize, minlon, minlat, xdim, ydim,\
+        d_grdlon, d_grdlat, d_count, d_avgtau, d_sumtau, d_sqrtau, d_stdtau, d_mintau, d_maxtau)
+
+    # Transfer data to host
+    avgtau = d_avgtau.copy_to_host().reshape([xdim,ydim])
+    stdtau = d_stdtau.copy_to_host().reshape([xdim,ydim])
+    grdlat = d_grdlat.copy_to_host().reshape([xdim,ydim])
+    grdlon = d_grdlon.copy_to_host().reshape([xdim,ydim])
+    mintau = d_mintau.copy_to_host().reshape([xdim,ydim])
+    maxtau = d_maxtau.copy_to_host().reshape([xdim,ydim])
+    count = d_count.copy_to_host().reshape([xdim,ydim])
+    sumtau = d_sumtau.copy_to_host().reshape([xdim,ydim])
+
     mintau[mintau==10]=None
     maxtau[maxtau==-1]=None
     avgtau[avgtau==-1]=None
-    
+
     return avgtau,stdtau,grdlat,grdlon,mintau,maxtau,count,sumtau
+
+@cuda.jit
+def grid_gpu_1(gsize, ydim, indata_length,\
+    indata, limit, inlat, inlon, sumtau, count, mintau, maxtau):
+    # Establish thread and block indices
+    t = cuda.threadIdx.x
+    #-------------------------
+    d = cuda.blockDim.x
+    #-------------------------
+    b = cuda.blockIdx.x
+
+    # Compute global array index
+    idx = t + b*d
+
+    # Unpack lat/lon limits
+    minlat=float(limit[0])
+    maxlat=float(limit[1])
+    minlon=float(limit[2])
+    maxlon=float(limit[3])    
+
+    # Grid values
+    if idx < indata_length:
+        if (inlat[idx]>=minlat and inlat[idx] <= maxlat and inlon[idx]>= minlon and inlon[idx]<= maxlon): # and indata[ii] >0.0 and indata[ii]<=5.0):
+            # pixel coordinates for bounds
+            i=int(round((inlon[idx]-minlon)/gsize))
+            j=int(round((inlat[idx]-minlat)/gsize))
+            ij = j + i*ydim
+            
+            # do not take nan values into account
+            # nans should not be counted
+            cuda.atomic.add(sumtau, ij, indata[idx])
+            cuda.atomic.add(sumtau, ij, indata[idx]**2)
+            cuda.atomic.add(count, ij, 1)
+            cuda.atomic.min(mintau, ij, indata[idx])
+            cuda.atomic.max(maxtau, ij, indata[idx])
+
+
+@cuda.jit
+def grid_gpu_2(gsize, minlon, minlat, xdim, ydim,\
+    grdlon, grdlat, count, avgtau, sumtau, sqrtau, stdtau, mintau, maxtau):
+    # Establish thread and block indices
+    tx = cuda.threadIdx.x
+    ty = cuda.threadIdx.y
+    #-------------------------
+    dx = cuda.blockDim.x
+    dy = cuda.blockDim.y
+    #-------------------------
+    bx = cuda.blockIdx.x
+    by = cuda.blockIdx.y
+
+    # Compute global array index
+    row = ty + by*dy
+    col = tx + bx*dx
+    idx = col + row*ydim
+
+    # Grid values
+    if row < xdim and col < ydim:
+        grdlon[idx] = gsize*row + minlon
+        grdlat[idx] = gsize*col + minlat
+        if count[idx] > 0:
+            avgtau[idx] = sumtau[idx] / count[idx]
+            para1=(1/count[idx])*(sqrtau[idx])+(count[idx])*avgtau[idx]-2*(avgtau[idx])*(sumtau[idx])
+            if para1 > 0:
+                stdtau[idx] = math.sqrt(para1)
+        
+        # Fill values
+        #if mintau[idx] == 10:
+        #    mintau[idx] = None
+        #if maxtau[idx] == -1:
+        #    maxtau[idx] = None
+        #if avgtau[idx] == -1:
+        #    avgtau[idx] = None
+
 
 # filelist - list of sensor names we want to grid here
 # modis - true false variables that tells us if a sensor is Modis or not
@@ -146,7 +272,7 @@ def multi_sensor_grid(filelist, gsize, limit, phy_list, geo_list=['latitude', 'l
     avgtau,stdtau,grdlat,grdlon,mintau,maxtau,count,sumtau = grid(limit,gsize,indata,inlat,inlon)
     
     return avgtau,stdtau,grdlat,grdlon,mintau,maxtau,count,sumtau
-
+    
 #grid(limit,gsize,indata,inlat,inlon)
 if __name__ == '__main__':
     limit = [-90., 90., -180., 180.]
@@ -161,4 +287,3 @@ if __name__ == '__main__':
     #print(avgtau)
     
     print(avgtau)
-    
