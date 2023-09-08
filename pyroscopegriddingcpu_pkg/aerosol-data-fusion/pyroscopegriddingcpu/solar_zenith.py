d5751254ff262a794da8dcce6237e27d8faf3deb
sally-zhao <67696901+sally-zhao@users.noreply.github.com>
2023-09-08 10:11:19 -0400

    Add files via upload
    Upload of all documents for the pyroscopegridding cpu version of the package (includes dist and egg files) version 0.0.0.9

---
+++ pyroscopegriddingcpu_pkg/aerosol-data-fusion/pyroscopegriddingcpu/solar_zenith.py
@@ -0,0 +1,196 @@
+
+__author__ = "Sally Zhao"
+__copyright__ = "Copyright 2023, Pyroscope"
+__credits__ = ["Neil Gutkin", "Jennifer Wei", "Pawan Gupta", "Robert Levy", "Xiaohua Pan", "Zhaohui Zhang"]
+__version__ = "1.0.0"
+__maintainer__ = "Sally Zhao"
+__email__ = "zhaosally0@gmail.com"
+__status__ = "Production"
+# Solar Zenith
+#
+# Calculates solar zenith and azimuth angles. 
+# Contains manual code calculations validated against pysolar
+#
+import math
+import datetime
+import numpy as np
+import pandas as pd
+
+from joblib import Parallel, delayed
+
+"""
+def get_SZA(row):
+    time_obj = pd.to_datetime(row['Date_UTC']) + pd.to_timedelta(row['Time_UTC'], unit='h')
+    time_obj = time_obj.replace(tzinfo=dt.timezone.utc)
+    lat, lon = row['Lat'], row['Lon']
+    
+    return get_altitude(lat, lon, time_obj)
+"""
+
+#global variables 
+rad = 180/math.pi
+
+### solar declination angle
+"""
+
+theta_0 = double(360.0 * double(D - 1) / 365.0) / rad
+
+declin  = 0.396372 - 22.91327*COS(theta_0) 			$
+	  + 4.02543*SIN(theta_0) 				$
+	  - 0.387205*COS(2.0*theta_0) 				$
+	  + 0.051967*SIN(2.0*theta_0) 				$
+	  - 0.154527*COS(3.0*theta_0) 				$
+	  + 0.084798*SIN(3.0*theta_0)
+
+declin = declin / rad	;convert to radians
+"""
+def solar_declination_angle(D):
+    theta_0 = (360.0 * (D - 1) / 365.0) / rad #converts to radians
+    declin  = (0.396372 - 22.91327*math.cos(theta_0)
+            + 4.02543*math.sin(theta_0) 
+            - 0.387205*math.cos(2.0*theta_0)
+            + 0.051967*math.sin(2.0*theta_0) 
+            - 0.154527*math.cos(3.0*theta_0) 
+            + 0.084798*math.sin(3.0*theta_0))
+    declin = declin / rad  #converts to radians
+    return theta_0, declin
+    
+### Time correction for solar angle
+"""    
+correct  = 0.004297 + 0.107029*COS(theta_0) 			$
+    - 1.837877*SIN(theta_0) - 0.837378*COS(2.0*theta_0) 	$
+    - 2.342824*SIN(2.0*theta_0)
+
+angle = (time - 12.0) * 15.0 + lon + correct
+IF angle GT  180.0 THEN angle = angle - 360.0
+IF angle LT -180.0 THEN angle = angle + 360.0
+
+lat_rad = lat / rad
+lon_rad = lon / rad
+
+angle_rad  = angle / rad
+"""
+def time_correction_solar_angle(theta_0, time, lat, lon): #assume theta_0 in radians
+    correct = (0.004297 + 0.107029*math.cos(theta_0)
+                - 1.837877*math.sin(theta_0)
+                - 0.837378*math.cos(2.0*theta_0)
+                - 2.342824*math.sin(2.0*theta_0))
+                    
+                
+    angle = (time - 12.0) * 15.0 + lon + correct
+    
+    if angle > 180:
+        angle = angle - 360
+    if angle < -180:
+        angle = angle + 360
+        
+    lat_rad = lat / rad
+    lon_rad = lon / rad
+
+    angle_rad  = angle / rad
+    
+    return angle, angle_rad, lat_rad, lon_rad
+
+
+### Sun zenith
+"""
+tmp1 = SIN(lat_rad)*SIN(declin) + COS(lat_rad)*COS(declin)*COS(angle_rad)
+tmp2   = ABS(tmp1)
+
+IF tmp2 GT 1.1 THEN BEGIN
+    PRINT, 'Error in acos argument in sun zenith'
+    PRINT, tmp1
+ENDIF ELSE BEGIN							$
+    IF tmp2 GT 1.0 THEN BEGIN
+	IF tmp1 GT 0.0 THEN tmp1=1.0
+	IF tmp1 LT 0.0 THEN tmp1=-1.0
+    ENDIF
+ENDELSE
+
+theta_rad = ACOS(tmp1)
+"""
+def sun_zenith(lat_rad, lon_rad, angle_rad, declin):
+    
+    #tmps are in radians
+    tmp1 = math.sin(lat_rad)*math.sin(declin) + math.cos(lat_rad)*math.cos(declin)*math.cos(angle_rad)
+    #tmp1 = (math.degrees(math.sin(lat_rad))* math.degrees(math.sin(declin)) 
+    #        + math.degrees(math.cos(lat_rad)) * math.degrees(math.cos(declin)) * math.degrees(math.cos(angle_rad)))
+    tmp2 = abs(tmp1)
+    
+    if tmp2 > 1.1:
+        print("Error in acos argument in sun zenith: ", tmp1)
+    else:
+        if tmp2 > 1.0:
+            if tmp1 > 0:
+                tmp1 = 1.0
+            if tmp1 < 0:
+                tmp1 = -1.0
+                
+    theta_rad = math.acos(tmp1)
+    
+    return theta_rad
+
+### Sun Azumith
+"""
+tmp1 = SIN(ABS(angle_rad)) *COS(declin) / SIN(theta_rad)
+azimuth_rad  = ASIN(tmp1)
+
+IF lat GT declin THEN azimuth_rad = 180.0 / rad - azimuth_rad
+IF angle  GT 0.0  THEN azimuth_rad = 360.0 / rad - azimuth_rad
+"""
+def sun_azimuth(lat, lon, angle, angle_rad, theta_rad, declin):
+    tmp1 = math.sin(abs(angle_rad)) * math.cos(declin) / math.sin(theta_rad)
+    azimuth_rad  = math.asin(tmp1)
+    
+    if lat > declin:
+        azimuth_rad = 180.0 / rad - azimuth_rad
+    if angle > 0.0:
+        azimuth_rad = 360.0 / rad - azimuth_rad
+        
+    return azimuth_rad
+
+#converts datetime object to numbers
+def date_to_num(datetime_obj):
+    D = datetime_obj.timetuple().tm_yday
+    time = datetime_obj.hour + (datetime_obj.minute / 60.0) + (datetime_obj.second / 3600)
+
+    return D, time
+
+# calculates solar zenith for singular point
+def get_SZA(lat, lon, time_start, time_diff=30):
+    datetime_obj = pd.to_datetime(time_start)
+    datetime_obj = datetime_obj.replace(tzinfo=datetime.timezone.utc)
+    
+    D, time = date_to_num(datetime_obj)
+    
+    theta_0, declin = solar_declination_angle(D)
+    angle, angle_rad, lat_rad, lon_rad = time_correction_solar_angle(theta_0, time, lat, lon)
+    theta_rad = sun_zenith(lat_rad, lon_rad, angle_rad, declin)
+    #azimuth_rad = sun_azimuth(lat, lon, angle, angle_rad, theta_rad, declin)
+    
+    return math.degrees(theta_rad)
+
+"""
+# uses pysolar library
+def get_SZA(lat, lon, time_start, time_diff):
+    time_obj = pd.to_datetime(time_start) + pd.to_timedelta(time_diff, unit="minute")
+    time_obj = time_obj.replace(tzinfo=datetime.timezone.utc)
+    
+    #return get_altitude(lat, lon, time_obj.to_pydatetime())
+    return (90-get_altitude(lat, lon, time_obj.to_pydatetime()))
+""" 
+
+#@jit(nopython=True)
+def get_SZA_parallelized(limit, gsize, time_start, time_diff,num_cores=1):
+    # limit = [min lat, max lat, min lon, max lon]
+    max_lat = int(1+round(((limit[1]-limit[0])/gsize)))
+    max_lon = int(1+round(((limit[3]-limit[2])/gsize)))
+        
+    res = Parallel(n_jobs = num_cores)( 
+            delayed( get_SZA )(limit[0] +  i * gsize, limit[2] + j * gsize, time_start, time_diff) 
+                                    for i in range(0, max_lat)
+                                    for j in range(0, max_lon)
+        )
+    
+    res = np.reshape(res, (-1, max_lon))
+    return res #solar_zenith
---
+++ pyroscopegriddingcpu_pkg/aerosol-data-fusion/pyroscopegriddingcpu/time_conv.py
@@ -0,0 +1,140 @@
+__author__ = "Sally Zhao"
+__copyright__ = "Copyright 2023, Pyroscope"
+__credits__ = ["Neil Gutkin", "Jennifer Wei", "Pawan Gupta", "Robert Levy", "Xiaohua Pan", "Zhaohui Zhang"]
+__version__ = "0.0.0.1"
+__maintainer__ = "Sally Zhao"
+__email__ = "zhaosally0@gmail.com"
+__status__ = "Production"
+# Time Conversion
+#
+# Buckets file names according to user specifications
+#
+
+# due to the introduction of the time variable, we need to know the time of the sensor data
+# this information is not given in the file itself, but rather the file name
+# e.g. "AERDT_L2_ABI_G16.A2021008.0850.001.nc" --> 10/08/2021 8:50 AM
+import datetime
+
+#from fileparser import *
+from pyroscopegriddingcpu.fileparser import *
+
+full_satellite_list = ['ABI_G16', 'ABI_G17', 'AHI_H08', 'VIIRS_SNPP', 'MOD04', 'MODIS_T', 'MODIS_A', 'MYD04']
+
+# given 'yyyy/mm/dd/hr/mm' string
+# converts to datetime object
+def to_datetime(date_string):
+    format = '%Y/%m/%d/%H/%M'
+    d = datetime.datetime.strptime(date_string, format)
+
+    return d
+
+# get system date
+def sys_time():
+    now = str(datetime.datetime.now())
+    now_str = now[:10]
+    now_str = now_str.replace("-", "")
+    
+    return str(now_str)
+
+# function that pulls time from filename (str)
+# returns datetime object
+def filename_time(filename):
+    #print("FILENAME: ", filename)
+    time = filename.split(".")[1:3]
+    date = datetime.datetime.strptime(time[0][3:], '%y%j')
+    
+    day_time = time[1]
+    date = date.replace(hour=int(day_time[:2]), minute=int(day_time[2:]))
+    
+    return date
+
+# given list of files, we need to sort them based upon their dates
+# input 1d array [[], [], []]
+# output 2d array where each element represents the list of dates that fall with the inteveral
+
+# start represents the start time (datetime object)
+# end represents the end time (datetime object)
+# start and end are inclusive (2/1/23 - should it be???)
+# interval is in minutes
+# intervals will be [start, start+interval] where first index is inclusive, second not
+def split_filetimes(filelist, start, end, interval):
+    split_files = []
+    curr = start
+    end_curr = start + datetime.timedelta(minutes = interval)
+    
+    while (curr <= end): # keep going while current starting time interval is valid
+        temp = []
+        for file in filelist:
+            if filename_time(file) >= curr and filename_time(file) < end_curr and filename_time(file) <= end:
+                temp.append(file)
+        curr = curr + datetime.timedelta(minutes = interval)
+        end_curr = end_curr + datetime.timedelta(minutes = interval)
+        
+        split_files.append(temp)
+    
+    return split_files
+
+# given a list of splitfiles (split on time)
+# e.g. : [[time 0], [time1], ... [timek]]
+# Additionally splits the inside lists based on common name
+# Creates dictionary where key is sensor name
+def split_filenames(filelist):
+    split_files = []
+    
+    for time in filelist: # time list
+        d = {} # dictionary
+
+        for f in time:
+            #retrieve sensor name from file format
+            f = convert_path_to_linux(f)
+            s_name = f.split("/")[-1].split(".")[0]
+            
+            #check to see if sensor is from list of the six measures satellites
+            for measures_satellite in full_satellite_list:
+                if measures_satellite in s_name:
+                    s_name = measures_satellite
+                    if measures_satellite == "MOD04" or measures_satellite == "MODIS_T":
+                        s_name = "MOD04"
+                    elif measures_satellite == "MYD04" or measures_satellite == "MODIS_A":
+                        s_name = "MYD04"
+                        
+            if s_name not in d: # add to dict if not already present
+                d[s_name] = [f]
+            else:
+                d[s_name].append(f)
+
+        #split_files.append(list(d.values()))
+        split_files.append(d)
+        
+    """
+    # missing files check
+    if len(split_files) < 6:
+        print("Missing sensors")
+    elif len(split_files)>6:
+        print("Too many sensors")
+    """
+    return split_files
+
+#testing
+if __name__ == '__main__':
+    files2019 = ["XAERDT_L2_ABI_G16.A2019208.0000.001.2022260004939.nc",
+                 "XAERDT_L2_ABI_G16.A2019208.0010.001.2022260004939.nc",
+                 "XAERDT_L2_ABI_G16.A2019208.0020.001.2022260004940.nc",
+                 "XAERDT_L2_ABI_G16.A2019208.0030.001.2022260004941.nc",
+                "XAERDT_L2_ABI_G16.A2019208.0040.001.2022260004942.nc",
+                "XAERDT_L2_ABI_G16.A2019208.0050.001.2022260004942.nc",
+                "XAERDT_L2_ABI_G16.A2019208.0100.001.2022260004939.nc"]
+    start = to_datetime("2019/07/27/00/00")
+    end = to_datetime("2019/07/27/00/40")
+    
+    files_split_time = split_filetimes(files2019, start, end, 30)
+    #print("Split on time: ", files_split_time)
+    
+    files_split_sensors = split_filenames(files_split_time)
+    
+    #print("\n\nSplit on sensors: ", files_split_sensors)
+    print(files_split_sensors)
+    
+    for key in files_split_sensors[0]:
+        print(key)
+    
